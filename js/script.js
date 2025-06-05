65169fcf57692166845d6755aae6e01f3054f4e9
Ellen2901 <rm566408@fiap.com.br>
2025-06-05 11:20:49 -0300

    nova feature de adição do botão de voltar ao topo da página através do javascript

--- js/script.js
+++ js/script.js
@@ -13,3 +13,20 @@ function nextImage(){
     document.getElementById("radio" + count).checked = true;
 }
 
+const btnTopo = document.getElementById("btnTopo");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    btnTopo.style.display = "block";
+  } else {
+    btnTopo.style.display = "none";
+  }               
+};
+
+function voltarAoTopo() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
